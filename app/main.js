98786edae683ff56a45d2f2e9d52622b54970ede
Gautham Srinivas <gsri0828@uni.sydney.edu.au>
2023-10-30 12:07:22 +1100

    added chatbot

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 // Replace this placeholder with your OpenAI API key
-const openAiApiKey = "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";
+const openAiApiKey = "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";
 
 // Initialize variables for context, set default values
 let userLocation = "Central Coast, NSW, Australia";
@@ -8,6 +8,7 @@ let currentWeather = "Sunny";
 
 // Initialize chatbot on page load
 document.addEventListener("DOMContentLoaded", function () {
+  
   addBotMessage("Hello again 👋");
   addBotMessage("Tell me if you are planning an event or finding cool spaces nearby.");
   document.getElementById('sendButton').addEventListener('click', sendMessage);
@@ -16,6 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
 // Function to send user message to bot
 async function sendMessage() {
   const input = document.getElementById('userInput').value;
+  console.log(input);
   if (input.trim() === '') return;
   addUserMessage(input);
 
@@ -31,24 +33,32 @@ async function sendMessage() {
 }
 
 // Function for OpenAI API call
-async function callOpenAiApi(input, apiKey, location, bodyTemp, weather) {
+async function callOpenAiApi(input, apiKey, location, currentTemp, weather) {
   // Prepare data for API call
   const data = {
-    model: "gpt-3.5-turbo-instruct",
-    prompt: `Location: ${location}\nBody Temperature: ${bodyTemp}\nWeather: ${weather}\nUser: ${input}`,
-    max_tokens: 50,
-    temperature: 0
+    model: "gpt-4",  // Specify GPT-4 model
+    messages: [
+      {
+        "role": "system",
+        "content": "You are a personalized heat mitigation strategist. Provide concise and personalised advice based on the user's request, utilising their real-time body temperature, location, and weather. Your responses must be within 40 words."
+      },
+      {
+        "role": "user",
+        "content": `Current Body Temperature: ${currentTemp}\nCurrent Location: ${location}\nCurrent Weather: ${weather}\n${input}`
+      }
+    ],
+    max_tokens: 50  // Limit to 50 tokens
   };
-  
-  // Make API call using Axios from CDN
+
+  // Make API call
   try {
-    const response = await axios.post('https://api.openai.com/v1/completions', data, {
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', data, {
       headers: {
         'Authorization': `Bearer ${apiKey}`,
         'Content-Type': 'application/json'
       }
     });
-    return response.data.choices[0].text.trim();
+    return response.data.choices[0].message.content.trim();
   } catch (error) {
     console.error(error);
     return 'An error occurred.';
