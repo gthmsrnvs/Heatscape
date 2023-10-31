8963d2deee5dd941d4c4fefa413a73b954ed38ec
Gautham Srinivas <gsri0828@uni.sydney.edu.au>
2023-10-31 14:29:07 +1100

    added chart finally, still need to update live

--- app/js/home.js
+++ app/js/home.js
@@ -1,16 +1,61 @@
-import { Chart } from 'chart.js';
+import Chart from 'chart.js/auto';
+import 'chartjs-adapter-date-fns';
+
+// Initialize the Chart.js chart
+const ctx = document.getElementById('temperatureChart').getContext('2d');
+const temperatureChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      label: 'Temperature',
+      data: [],
+      borderColor: 'rgba(75, 192, 192, 1)',
+      borderWidth: 1,
+      fill: false
+    }]
+  },
+  options: {
+    scales: {
+      x: {
+        type: 'time',
+        time: {
+          unit: 'second'
+        }
+      },
+      y: {
+        min: 0,
+        max: 100
+      }
+    }
+  }
+});
 
 // Function to handle temperature change
 function handleTemperatureChange(event) {
   const value = event.target.value;
-  const tempAsFloat = value.getFloat32(0, true); // read as little-endian
-  console.log(`Received ${tempAsFloat}`);
-  
-  // Update the temperature value in the <h1> element
-  const temperatureValueElement = document.getElementById('temperatureValue');
-  temperatureValueElement.innerText = `Temperature: ${tempAsFloat} °C`;
+  const tempAsFloat = value.getFloat32(0, true);
+
+  // Update the <h1> element with the temperature value
+  document.getElementById('temperatureValue').innerText = `Temperature: ${tempAsFloat}`;
+
+  // Update the Chart.js data
+  temperatureChart.data.labels.push(new Date().toLocaleTimeString());
+  temperatureChart.data.datasets[0].data.push(tempAsFloat);
+
+  // Limit the number of data points displayed to maintain a smooth real-time graph
+  const maxDataPoints = 10;
+  if (temperatureChart.data.labels.length > maxDataPoints) {
+    temperatureChart.data.labels.shift();
+    temperatureChart.data.datasets[0].data.shift();
+  }
+
+  // Update the Chart.js chart
+  temperatureChart.update();
 }
 
+// Add an event listener to connect and start receiving data when the button is clicked
+document.getElementById('connectButton').addEventListener('click', connectAndReceiveData);
 
 // Function to connect to the device and start receiving data
 function connectAndReceiveData() {
@@ -43,6 +88,3 @@ function connectAndReceiveData() {
       console.log('Error:', error);
     });
 }
-
-// Add an event listener to connect and start receiving data when the button is clicked
-document.getElementById('connectButton').addEventListener('click', connectAndReceiveData);
