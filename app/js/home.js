f7295e86976df6d5f74be9d831dea2dac94291ee
Gautham Srinivas <gsri0828@uni.sydney.edu.au>
2023-10-31 13:52:56 +1100

    fixed main

--- app/js/home.js
+++ app/js/home.js
@@ -1,92 +1,41 @@
-// Import Chart.js
-import Chart from 'chart.js';
-
-// Initialize the Chart.js configuration
-const ctx = document.getElementById('temperatureChart').getContext('2d');
-const temperatureChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: [],
-    datasets: [{
-      label: 'Temperature',
-      data: [],
-      fill: false,
-      borderColor: 'rgb(75, 192, 192)',
-      tension: 0.1
-    }]
-  },
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-      x: {
-        type: 'time',
-        time: {
-          unit: 'second'
-        },
-        position: 'bottom'
-      },
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
-
 // Function to handle temperature change
-function handleTemperatureChange(tempAsFloat) {
-  // Update the <h1> element with the temperature value
-  document.getElementById('temperatureValue').innerText = tempAsFloat;
-
-  // Update the Chart.js data
-  const timestamp = new Date().toISOString();
-  temperatureChart.data.labels.push(timestamp);
-  temperatureChart.data.datasets[0].data.push({ x: timestamp, y: tempAsFloat });
-
-  // Limit the number of data points displayed
-  const maxDataPoints = 10;
-  if (temperatureChart.data.labels.length > maxDataPoints) {
-    temperatureChart.data.labels.shift();
-    temperatureChart.data.datasets[0].data.shift();
-  }
-
-  // Update the Chart.js chart
-  temperatureChart.update();
+function handleTemperatureChange(event) {
+  const value = event.target.value;
+  const tempAsFloat = value.getFloat32(0, true); // read as little-endian
+  console.log(`Received ${tempAsFloat}`);
 }
 
 // Function to connect to the device and start receiving data
 function connectAndReceiveData() {
   navigator.bluetooth
     .requestDevice({ filters: [{ services: ['12345678-1234-5678-1234-56789abcdef0'] }] })
-    .then(device => {
+    .then((device) => {
       console.log('Got device:', device.name);
       return device.gatt.connect();
     })
-    .then(server => {
+    .then((server) => {
       return server.getPrimaryService('12345678-1234-5678-1234-56789abcdef0');
     })
-    .then(service => {
+    .then((service) => {
       return service.getCharacteristic('12345678-1234-5678-1234-56789abcdef1');
     })
-    .then(characteristic => {
-      // Initial read
-      characteristic.readValue().then(value => {
-        const tempAsFloat = value.getFloat32(0, true);
-        handleTemperatureChange(tempAsFloat);
+    .then((characteristic) => {
+      // Read the value initially
+      characteristic.readValue().then((value) => {
+        handleTemperatureChange({ target: { value } });
       });
 
-      // Periodic updates
+      // Set up an interval to read and update the value every 2 seconds
       setInterval(() => {
-        characteristic.readValue().then(value => {
-          const tempAsFloat = value.getFloat32(0, true);
-          handleTemperatureChange(tempAsFloat);
+        characteristic.readValue().then((value) => {
+          handleTemperatureChange({ target: { value } });
         });
-      }, 2000); // Update every 2 seconds
+      }, 2000); // 2000 milliseconds = 2 seconds
     })
-    .catch(error => {
+    .catch((error) => {
       console.log('Error:', error);
     });
 }
 
-// Add an event listener to start the process when the button is clicked
+// Add an event listener to connect and start receiving data when the button is clicked
 document.getElementById('connectButton').addEventListener('click', connectAndReceiveData);
