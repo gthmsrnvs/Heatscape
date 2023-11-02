a6762b639fc2856027ac07bbf00a4e8622e1ea21
BikeMcBikerson <jamesmarsuniversalis@gmail.com>
2023-11-02 11:58:15 +1100

    Separated Weather and Graph code + Added Scrollbooster
    Removed home.js and replaced it with weather.js and graph.js
    
    Added scrollbooster library for easy horizontal dragging

--- app/js/components.js
+++ app/js/components.js
@@ -1,29 +1,39 @@
 const navbar = document.getElementById('navbar')
+const navbarItems = navbar.getElementsByClassName('navbarItem')
 
+// Set the navbar
 navbar.innerHTML = `
-
 <a href="index.html"  class="navbarItem">
     <i class="fa-solid fa-house"></i>
     <span>
         Home
     </span>
 </a>
-<a href="index.html" class="navbarItem">
+<a href="chatbot.html" class="navbarItem">
     <i class="fa-solid fa-lightbulb"></i>
     <span>
         Help
     </span>
 </a>
-<a href="index.html" class="navbarItem">
+<a href="#" class="navbarItem">
     <i class="fa-solid fa-location-dot"></i>
     <span>
         Map
     </span>
 </a>
-<a href="index.html" class="navbarItem">
+<a href="#" class="navbarItem">
     <i class="fa-solid fa-gear"></i>
     <span>
         Settings
     </span>
 </a>
 `
+
+// Set the active class to the current page
+let url = window.location.pathname;
+for (let i = 0; i < navbarItems.length; i++) {
+    let navbarItemsHref = navbarItems[i].href.replace("http://localhost:5173", "")
+    if (url === navbarItemsHref) {
+        navbarItems[i].style.setProperty("color", "var(--primary)");
+    }
+}
