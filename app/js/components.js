2735dda367af11f082a16db2c6a57e4e1863b817
Anson Chew <ansonchewhl@gmail.com>
2023-11-07 13:02:52 +1100

    Updated the navbar items to link the Figma pages

--- app/js/components.js
+++ app/js/components.js
@@ -9,19 +9,19 @@ navbar.innerHTML = `
         Home
     </span>
 </a>
-<a href="chatbot.html" class="navbarItem">
+<a href="https://www.figma.com/proto/f08fZuaZlyEmnoRLUH5ViL/DECO3200-Iteration-6?type=design&node-id=22675-6024&t=QWBPD5fZL8SYN6QQ-0&scaling=scale-down&page-id=5322%3A0&starting-point-node-id=22675%3A5944" class="navbarItem">
     <i class="fa-solid fa-lightbulb"></i>
     <span>
         Help
     </span>
 </a>
-<a href="#" class="navbarItem">
+<a href="https://www.figma.com/proto/f08fZuaZlyEmnoRLUH5ViL/DECO3200-Iteration-6?type=design&node-id=22754-11634&t=QWBPD5fZL8SYN6QQ-0&scaling=scale-down&page-id=5322%3A0&starting-point-node-id=22675%3A5944" class="navbarItem">
     <i class="fa-solid fa-location-dot"></i>
     <span>
         Map
     </span>
 </a>
-<a href="#" class="navbarItem">
+<a href="https://www.figma.com/proto/f08fZuaZlyEmnoRLUH5ViL/DECO3200-Iteration-6?type=design&node-id=23578-11141&t=QWBPD5fZL8SYN6QQ-0&scaling=scale-down&page-id=5322%3A0" class="navbarItem">
     <i class="fa-solid fa-gear"></i>
     <span>
         Settings
@@ -34,7 +34,7 @@ let url = window.location.pathname;
 if (url === "/") url += "index.html";
 
 for (let i = 0; i < navbarItems.length; i++) {
-    let navbarItemsHref = navbarItems[i].href.replace("http://localhost:5173", "")
+    let navbarItemsHref = navbarItems[i].href.substring(navbarItems[i].href.lastIndexOf('/'));
 
     if (url === navbarItemsHref) {
         navbarItems[i].style.setProperty("color", "var(--primary)");
